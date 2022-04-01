0d7530b8fa4b091deafa1dcb7fdadc0eb7a358bb
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-01 20:41:39 +0600

    simple-Animaion-Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MyLineChart from './Components/MyLineChart/MyLineChart';
 import SpecialChart from './Components/SpecialChart/SpecialChart';
+import SpringAnimation from './Components/SpringAnimation/SpringAnimation';
 
 function App() {
   return (
     <div className="App">
+      <SpringAnimation></SpringAnimation>
       <MyLineChart></MyLineChart>
       <SpecialChart></SpecialChart>
 
